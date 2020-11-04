6474012c99ed7f69f57be700724b8975333ce26e
Damilare <damilareoyetade1@gmail.com>
2020-11-04 05:17:17 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+
+function changeBodyBg(color){
+    document.body.style.background = color;
+    document.getElementById('middle').style.background = color;
+    document.getElementById('right').style.background = color;
+
+}
